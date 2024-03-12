09f537ac2b0bf87e2fcb3aef421de83dbf2bdc63
Ala <ala@Alas-MacBook-Pro.local>
2024-03-12 15:44:25 +0200

    fixed path

--- e02/src/main/java/myapp/Library.java
+++ e02/src/main/java/myapp/Library.java
@@ -13,7 +13,7 @@ public class Library {
     private List<Book> books = new ArrayList<>();
 
     public void readBooksFomFile() throws IOException {
-        try (Stream<String> st = Files.lines(Path.of("/Users/ala/2024-wk10-java-optionals-and-streams-AlicjaKo/e02/src/main/resources/books.txt"))) {
+        try (Stream<String> st = Files.lines(Path.of("../../resources/books.txt"))) {
             st
             .map(line -> line.split(", "))
             .forEach(el -> {
